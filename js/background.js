d3157ed6433a8c00063a902fc38c263a7b577546
JaehyunLee123 <jhyon123@gmail.com>
2023-01-28 10:50:19 +0900

    background resizing

--- js/background.js
+++ js/background.js
@@ -1,11 +1,23 @@
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
 const choosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+
+const setBackgroundImageSize = () => {
+  if (window.innerHeight * 1.6 > window.innerWidth) {
+    bgImage.classList.remove("set-size-by-width");
+    bgImage.classList.add("set-size-by-height");
+  } else {
+    bgImage.classList.remove("set-size-by-height");
+    bgImage.classList.add("set-size-by-width");
+  }
+};
 
 bgImage.src = `img/${choosenImage}`;
 
 document.body.appendChild(bgImage);
 
 bgImage.classList.add("background-image");
+
+window.addEventListener("resize", setBackgroundImageSize);
